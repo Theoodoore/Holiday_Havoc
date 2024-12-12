7acdf876ad1182d90e8ba3b5579ea04d468ef5aa
Fabian-Skyner <40596430@live.napier.ac.uk>
2024-12-12 00:39:43 +0000

    Update scene_menu.cpp
    second attempt to get this to render, made with as much hubris as possible

--- Holiday_Havoc/scenes/scene_menu.cpp
+++ Holiday_Havoc/scenes/scene_menu.cpp
@@ -3,13 +3,15 @@
 #include "../game.h"
 #include <SFML/Window/Keyboard.hpp>
 #include <iostream>
+#include "engine.h"
 
 using namespace std;
 using namespace sf;
 
 void MenuScene::Load() {
-  cout << "Menu Load \n";
-  {
+	cout << "Menu Load \n";
+	
+	
 	sf::Texture* titleScreenTexture = new sf::Texture();
     if (!titleScreenTexture->loadFromFile("res/img/title_screen.png")) {
 		throw std::runtime_error("Failed to load title screen texture!");
@@ -20,10 +22,12 @@ void MenuScene::Load() {
 	Vector2u textureSize = titleScreenTexture->getSize();
 	titleScreenSprite.setOrigin(textureSize.x / 2, textureSize.y / 2);
 	
+	sf::RenderWindow& window = Engine::GetWindow();
+	window.draw(titleScreenSprite);
+	
     //auto txt = makeEntity();
     //auto t = txt->addComponent<TextComponent>(
-    //    "Holiday Havoc\n\nPress Space to Start");
-	}
+	//    "Holiday Havoc\n\nPress Space to Start");
 	setLoaded(true);
 }
 
