66af6f8d5bd547963b29a796500fc7791b0749fb
Fabian-Skyner <40596430@live.napier.ac.uk>
2024-12-12 00:16:17 +0000

    began menu background implementation
    replaces text in main menu with a sprite that doesn't render

--- Holiday_Havoc/scenes/scene_menu.cpp
+++ Holiday_Havoc/scenes/scene_menu.cpp
@@ -10,11 +10,21 @@ using namespace sf;
 void MenuScene::Load() {
   cout << "Menu Load \n";
   {
-    auto txt = makeEntity();
-    auto t = txt->addComponent<TextComponent>(
-        "Holiday Havoc\n\nPress Space to Start");
-  }
-  setLoaded(true);
+	sf::Texture* titleScreenTexture = new sf::Texture();
+    if (!titleScreenTexture->loadFromFile("res/img/title_screen.png")) {
+		throw std::runtime_error("Failed to load title screen texture!");
+    }
+	sf::Sprite titleScreenSprite;
+	titleScreenSprite.setTexture(*titleScreenTexture);
+	
+	Vector2u textureSize = titleScreenTexture->getSize();
+	titleScreenSprite.setOrigin(textureSize.x / 2, textureSize.y / 2);
+	
+    //auto txt = makeEntity();
+    //auto t = txt->addComponent<TextComponent>(
+    //    "Holiday Havoc\n\nPress Space to Start");
+	}
+	setLoaded(true);
 }
 
 void MenuScene::Update(const double& dt) {
